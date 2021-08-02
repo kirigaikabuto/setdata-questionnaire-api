987abf0088220c52b6e20967068f3b4f4654149c
Yerassyl <tleugazy98@gmail.com>
2021-08-02 21:21:30 +0600

    commits

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/kirigaikabuto/setdata-questionnaire-api
+
+go 1.13
+
+require (
+	github.com/djumanoff/amqp v1.0.6
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.3.0
+	github.com/kirigaikabuto/setdata-common v1.0.1-0.20210802114433-ce62b657191a
+	github.com/kirigaikabuto/setdata_questionnaire v0.0.2-0.20210802151922-a9ad8b7d5ed1
+	github.com/urfave/cli v1.22.5
+)
---
+++ main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"github.com/djumanoff/amqp"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	setdata_common "github.com/kirigaikabuto/setdata-common"
+	"github.com/kirigaikabuto/setdata_questionnaire"
+	"github.com/urfave/cli"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+var (
+	configPath           = ".env"
+	version              = "0.0.0"
+	amqpHost             = ""
+	amqpPort             = 0
+	postgresUser         = ""
+	postgresPassword     = ""
+	postgresDatabaseName = ""
+	postgresHost         = ""
+	postgresPort         = 5432
+	postgresParams       = ""
+	flags                = []cli.Flag{
+		&cli.StringFlag{
+			Name:        "config, c",
+			Usage:       "path to .env config file",
+			Destination: &configPath,
+		},
+	}
+)
+
+func parseEnvFile() {
+	// Parse config file (.env) if path to it specified and populate env vars
+	if configPath != "" {
+		godotenv.Overload(configPath)
+	}
+	amqpHost = os.Getenv("RABBIT_HOST")
+	amqpPortStr := os.Getenv("RABBIT_PORT")
+	amqpPort, _ = strconv.Atoi(amqpPortStr)
+	if amqpPort == 0 {
+		amqpPort = 5672
+	}
+	if amqpHost == "" {
+		amqpHost = "localhost"
+	}
+	postgresUser = os.Getenv("POSTGRES_USER")
+	postgresPassword = os.Getenv("POSTGRES_PASSWORD")
+	postgresDatabaseName = os.Getenv("POSTGRES_DATABASE")
+	postgresParams = os.Getenv("POSTGRES_PARAMS")
+	portStr := os.Getenv("POSTGRES_PORT")
+	postgresPort, _ = strconv.Atoi(portStr)
+	postgresHost = os.Getenv("POSTGRES_HOST")
+	if postgresHost == "" {
+		postgresHost = "localhost"
+	}
+	if postgresPort == 0 {
+		postgresPort = 5432
+	}
+	if postgresUser == "" {
+		postgresUser = "setdatauser"
+	}
+	if postgresPassword == "" {
+		postgresPassword = "123456789"
+	}
+	if postgresDatabaseName == "" {
+		postgresDatabaseName = "setdata"
+	}
+	if postgresParams == "" {
+		postgresParams = "sslmode=disable"
+	}
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "Set data questions api"
+	app.Description = ""
+	app.Usage = "set data run"
+	app.UsageText = "set data run"
+	app.Version = version
+	app.Flags = flags
+	app.Action = run
+
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func run(c *cli.Context) error {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	parseEnvFile()
+	amqpConfig := amqp.Config{
+		Host: amqpHost,
+		Port: amqpPort,
+	}
+	sess := amqp.NewSession(amqpConfig)
+	err := sess.Connect()
+	if err != nil {
+		return err
+	}
+	clt, err := sess.Client(amqp.ClientConfig{})
+	if err != nil {
+		return err
+	}
+	amqpRequests := setdata_questionnaire.NewAmqpRequests(clt)
+	service := setdata_questionnaire.NewQuestionsService(amqpRequests)
+	httpEndpoints := setdata_questionnaire.NewHttpEndpoints(setdata_common.NewCommandHandler(service))
+	router := mux.NewRouter()
+
+	router.Methods("POST").Path("/questions").HandlerFunc(httpEndpoints.MakeCreateQuestionEndpoint())
+	router.Methods("PUT").Path("/questions").HandlerFunc(httpEndpoints.MakeUpdateQuestionEndpoint())
+	router.Methods("GET").Path("/questions").HandlerFunc(httpEndpoints.MakeListQuestionEndpoint())
+	router.Methods("PUT").Path("/questions/add").HandlerFunc(httpEndpoints.MakeAddFieldToQuestionEndpoint())
+	http.ListenAndServe(":"+port, router)
+	return nil
+}
